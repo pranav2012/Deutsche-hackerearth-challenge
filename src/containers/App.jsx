56d0b335c0b087e5bc989b44a4274ec6011e8591
pranav2012 <p2012agarwal@gmail.com>
2022-06-04 16:42:02 +0530

    integrated pages

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -8,15 +8,22 @@ import Login from "../components/Login";
 import { Header } from "../components/Header";
 import { Footer } from "../components/Footer";
 import Article from "../components/Article";
+import Register from "../components/Register";
+import { useState } from "react";
+import ErrorPage from "../components/ErrorPage";
 
 export default function App() {
+  const [isloggedin, setisloggedin] = useState(false);
+
   return (
     <BrowserRouter>
-      <Header/>
+      <Header isloggedin={isloggedin}/>
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Blog />} />
+        <Route path="/" element={<Blog/>} />
+        <Route path="/login" element={isloggedin ? <Blog/> : <Login setisloggedin={setisloggedin}/>} />
+        <Route path="/register" element={isloggedin ? <Blog/> : <Register setisloggedin={setisloggedin}/>} />
         <Route path="/article" element={<Article />} />
+        <Route path="*" element={<ErrorPage/>} />
       </Routes>
       <Footer/>
     </BrowserRouter>
